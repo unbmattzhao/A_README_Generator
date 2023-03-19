1c11651888d9e8467f61bd03373766a7ca593d36
Matt Zhao <46049501+JediRebel@users.noreply.github.com>
2023-03-19 19:33:13 -0300

    more codes and markdown

---
+++ index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = rerquire("path");
+const inquirer = require("inquirer");
+const generateReadme = require("");
+
+const questions = [];
+
+function writeFile() {}
+function init() {}
+
+init();
